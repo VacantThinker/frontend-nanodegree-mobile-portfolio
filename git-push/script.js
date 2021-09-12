29dde4f92a07091a14ca6e510c578bdb5452b1df
VacantThinker <littlehappyforsmile@outlook.com>
2021-09-12 17:58:44 +0800

    update

--- git-push/script.js
+++ git-push/script.js
@@ -1,3 +1,3 @@
 const execSync = require('child_process').execSync;
 const mssg = 'BUILD - ' + process.argv[2];
-execSync(' git add . && git commit -m "update" ', { stdio:[0, 1, 2] });
+execSync(' git add . && git commit -m "update" && git push', { stdio:[0, 1, 2] });
