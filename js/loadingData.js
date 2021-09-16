eb857f197af2c95c0f59ec921e3f1dd00aaaf5cb
VacantThinker <50100840+VacantThinker@users.noreply.github.com>
2021-09-17 06:05:31 +0800

    'update'

--- js/loadingData.js
+++ js/loadingData.js
@@ -7,14 +7,14 @@ window.addEventListener("DOMContentLoaded", async () => {
             })
             .then(data => {
                 console.log("data", data)
-                document.querySelectorAll(".hero")[0]
+                document.querySelectorAll(".container_hero")[0]
                     .innerHTML = data.hero.html;
 
-                document.querySelectorAll(".content ul li a")
+                document.querySelectorAll(".container_content ul li a")
                     .forEach((ele, index)=>{
                         ele.innerHTML = data.content[index].aContent;
                     })
-                document.querySelectorAll(".content ul li p")
+                document.querySelectorAll(".container_content ul li p")
                     .forEach((ele, index)=>{
                         ele.innerHTML = data.content[index].pContent;
                     })
