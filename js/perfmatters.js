330669014f1b0076c3ca470809de7f9feb3044fb
VacantThinker <50100840+VacantThinker@users.noreply.github.com>
2021-09-16 11:45:42 +0800

    'update'

--- js/perfmatters.js
+++ js/perfmatters.js
@@ -1,16 +1,18 @@
 // Measuring the Critical Rendering Path with Navigation Timing
 // https://developers.google.com/web/fundamentals/performance/critical-rendering-path/measure-crp
 
-// var startTime;
-// window.addEventListener("DOMContentLoaded", function (e) {
-//   startTime = performance.now();
-// })
+var startTime;
+window.addEventListener("DOMContentLoaded", function (e) {
+  startTime = performance.now();
+})
 
-// window.addEventListener("load", function(event) {
+window.addEventListener("load", function(event) {
+  var endTime = performance.now();
+
   var stats = document.getElementById("crp-stats");
 // stats.textContent = 'DCL: ' + dcl + 'ms, onload: ' + complete + 'ms';
-  stats.textContent = 'DCL:'+Math.floor(window.startTime) + 'ms' +' - '+ 'load:' + Math.floor(window.endTime) + 'ms';
-  window.startTime = null;
-  window.endTime = null;
+  stats.textContent = 'DCL:'+Math.floor(startTime) + 'ms' +' - '+ 'load:' + Math.floor(endTime) + 'ms';
+  startTime = null;
+  endTime = null;
   stats = null;
-// });
+});
