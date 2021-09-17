c940a7e479e3ad2d71ae53c812d1c7bf815562aa
VacantThinker <50100840+VacantThinker@users.noreply.github.com>
2021-09-17 17:49:32 +0800

    'update'

--- js/loadingData.js
+++ js/loadingData.js
@@ -1,28 +1,37 @@
 // window.addEventListener("DOMContentLoaded", async () => {
 
-    async function reqData() {
-        await fetch('text/data.json')
-            .then(response => {
-                return response.json();
-            })
-            .then(data => {
-                // console.log("data", data)
-                document.querySelectorAll(".container_hero")[0]
-                    .innerHTML = data.hero.html;
+function appendDataToHtml(data) {
+    document.querySelectorAll("header > .header_info")[0]
+        .innerHTML = data.header.info
 
-                document.querySelectorAll(".container_content_li_right_a")
-                    .forEach((ele, index)=>{
-                        ele.innerHTML = data.content[index].aContent;
-                    })
-                document.querySelectorAll(".container_content_li_right_p")
-                    .forEach((ele, index)=>{
-                        ele.innerHTML = data.content[index].pContent;
-                    })
+    document.querySelectorAll(".container_hero")[0]
+        .innerHTML = data.hero.html;
 
+    document.querySelectorAll(".container_content_li_right_a")
+        .forEach((ele, index) => {
+            ele.innerHTML = data.content[index].aContent;
+        })
+    document.querySelectorAll(".container_content_li_right_p")
+        .forEach((ele, index) => {
+            ele.innerHTML = data.content[index].pContent;
+        })
+}
 
-            })
-    }
-    reqData().then(null);
+async function reqData() {
+    await fetch('text/data.json')
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            // console.log("data", data)
+            appendDataToHtml(data);
+        })
+        .catch(reason => {
+            throw Error(reason)
+        })
+}
 
-    // await reqData().then(r => null);
+reqData().then(null);
+
+// await reqData().then(r => null);
 // })
